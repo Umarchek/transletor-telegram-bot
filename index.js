7b55d9a2f16ec62ec809d250eda306e9b03d4894
UZBase <sharofiddinov.07@mail.ru>
2021-11-14 11:43:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,66 @@ const TOKEN = "2137017839:AAF_LjKwvHyAcCJ7kSmvoQXVI1a7gfBJNOE"
 const bot = new TelegramBot(TOKEN, {
     polling: true
 })
+const translate = require('@vitalets/google-translate-api')
+const { findUser, createUser, setText } = require('./model')
+
+bot.on('message', async (message) => {
+    const chat_id = message.chat.id
+    const message_id = message.message_id
+    const name = message.from.first_name
+    const text = message.text
+
+    let user = await findUser(chat_id)
+
+    if (!user) {
+        await createUser(chat_id)
+        await bot.sendMessage(chat_id, `Birorta so'z kiriting!`)
+    } else {
+
+        const keyboard = {
+            inline_keyboard: [
+                [
+                    {
+                        text: "UZ",
+                        callback_data: 'uz'
+                    },
+                    {
+                        text: "RU",
+                        callback_data: 'ru'
+                    },
+                ],
+                [
+                    {
+                        text: "EN",
+                        callback_data: 'en'
+                    },
+                    {
+                        text: "AR",
+                        callback_data: 'ar'
+                    },
+                ],
+            ]
+        }
+
+        await setText(chat_id, text)
+        await bot.sendMessage(chat_id, text, {
+            reply_markup: keyboard
+        })
+    }
+
+})
+
+bot.on('callback_query', async (query) => {
+    const data = query.data
+    const chat_id = query.from.id
+    const text = query.message.text
+
+    let user = await findUser(chat_id)
+
+    let translatedWord = await translate(text, { to: data })
+
+    await bot.editMessageText(translatedWord.text, {
+        chat_id,
+        message_id: query.message.message_id
+    })
+})
