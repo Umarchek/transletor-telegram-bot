4c4b448bb1a008764631c390c9e6cb6fc49593d9
UZBase <sharofiddinov.07@mail.ru>
2021-11-14 12:09:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api')
-const TOKEN = "****"
+const TOKEN = "***"
 
 const translate = require('@vitalets/google-translate-api')
 const { findUser, createUser, setText } = require('./models/model')
@@ -10,63 +10,88 @@ const bot = new TelegramBot(TOKEN, {
     polling: true
 })
 
-bot.on('message', async (message) => {
-    const chat_id = message.chat.id
-    const message_id = message.message_id
-    const name = message.from.first_name
-    const text = message.text
 
-    let user = await findUser(chat_id)
-
-    if (!user) {
-        await createUser(chat_id)
-        await bot.sendMessage(chat_id, `Birorta so'z kiriting!`)
-    } else {
-
-        const keyboard = {
-            inline_keyboard: [
-                [
-                    {
-                        text: "UZ",
-                        callback_data: 'uz'
-                    },
-                    {
-                        text: "RU",
-                        callback_data: 'ru'
-                    },
-                ],
-                [
-                    {
-                        text: "EN",
-                        callback_data: 'en'
-                    },
-                    {
-                        text: "AR",
-                        callback_data: 'ar'
-                    },
-                ],
-            ]
-        }
-
-        await setText(chat_id, text)
-        await bot.sendMessage(chat_id, text, {
-            reply_markup: keyboard
-        })
-    }
-
-})
-
-bot.on('callback_query', async (query) => {
-    const data = query.data
-    const chat_id = query.from.id
-    const text = query.message.text
-
-    let user = await findUser(chat_id)
-
-    let translatedWord = await translate(text, { to: data })
-
-    await bot.editMessageText(translatedWord.text, {
-        chat_id,
-        message_id: query.message.message_id
+if ('/start') {
+    bot.on('message', async (message) => {
+        const chat_id = message.chat.id
+        const { chat, message_id } = message;
+        const name = message.from.first_name;
+        bot.sendMessage(chat_id,
+            `Salom <b>${name}</b>. Bu bot orqali siz o'zizga kerak bo'lgan so'zni tarjima qilishingiz mumkun!`,
+            {
+                parse_mode: "HTML",
+                reply_markup: {
+                    inline_keyboard: [
+                        [
+                            {
+                                text: `Tarjima bulimiga`,
+                                callback_data: `start`,
+                            },
+                        ],
+                    ],
+                },
+            }
+        );
     })
-})
+}
+else {
+    bot.on('message', async (message) => {
+        const chat_id = message.chat.id
+        const message_id = message.message_id
+        const name = message.from.first_name
+        const text = message.text
+
+        let user = await findUser(chat_id)
+
+        if (!user) {
+            await createUser(chat_id)
+            await bot.sendMessage(chat_id, `Birorta so'z kiriting!`)
+        } else {
+
+            const keyboard = {
+                inline_keyboard: [
+                    [
+                        {
+                            text: "UZ",
+                            callback_data: 'uz'
+                        },
+                        {
+                            text: "RU",
+                            callback_data: 'ru'
+                        },
+                    ],
+                    [
+                        {
+                            text: "EN",
+                            callback_data: 'en'
+                        },
+                        {
+                            text: "AR",
+                            callback_data: 'ar'
+                        },
+                    ],
+                ]
+            }
+
+            await setText(chat_id, text)
+            await bot.sendMessage(chat_id, text, {
+                reply_markup: keyboard
+            })
+        }
+    })
+
+    bot.on('callback_query', async (query) => {
+        const data = query.data
+        const chat_id = query.from.id
+        const text = query.message.text
+
+        let user = await findUser(chat_id)
+
+        let translatedWord = await translate(text, { to: data })
+
+        await bot.editMessageText(translatedWord.text, {
+            chat_id,
+            message_id: query.message.message_id
+        })
+    })
+}
